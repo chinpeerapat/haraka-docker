5bf43e833162cbf9c01649c9a2e701fdf3286b38
chinpeerapat <88846925+chinpeerapat@users.noreply.github.com>
2024-11-14 02:09:16 +0700

    Update start-haraka.sh

--- start-haraka.sh
+++ start-haraka.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # description: Haraka startup and configuration script
 
+# Link or copy TLS files if they don’t already exist in /haraka/config
+[ ! -f /haraka/config/tls_cert.pem ] && ln -s /haraka/config/tls/tls_cert.pem /haraka/config/tls_cert.pem
+[ ! -f /haraka/config/tls_key.pem ] && ln -s /haraka/config/tls/tls_key.pem /haraka/config/tls_key.pem
+
+# Run the configuration script
 /usr/local/bin/haraka-configure --haraka-config-dir /haraka/config --haraka-config-templates-dir /haraka/templates
 
-## start haraka
+# Start Haraka
 /usr/local/bin/haraka -c /haraka
